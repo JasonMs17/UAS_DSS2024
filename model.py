f6b4974f1c1151df42c904f543e74094d09933db
faizfhri <bluemlkyfaiz@gmail.com>
2024-12-15 17:16:16 +0700

    Ganti Metode ke Random Forest

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
@@ -27,7 +27,7 @@ def load_model():
     encoders['Sleep Disorder'] = le_sleep_disorder
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = GaussianNB()
+    model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
     accuracy = accuracy_score(y_test, model.predict(X_test))
     return model, encoders, accuracy
